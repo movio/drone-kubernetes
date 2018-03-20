5851a12735170284368db52ec5439b25132a6659
hashfyre <joy.bhattacherjee@razorpay.com>
2018-03-20 16:17:25 +0530

    scipts: run: adds support for PLUGIN_KIND -> JOB, DEPLOYMENT

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,11 +3,23 @@ set -euo pipefail
 
 BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )
 
+KUBE_KIND=${PLUGIN_KIND}
+
 source ${BASEDIR}/params.sh
 source ${BASEDIR}/cluster-auth.sh
-source ${BASEDIR}/deploy.sh
 
 setGlobals
 clusterAuth ${SERVER_URL} ${CLUSTER} ${USER}
 setContext ${CLUSTER} ${USER}
-startDeployments ${CLUSTER} ${NAMESPACE}
+
+if [[ ${KUBE_KIND} == "deployment" ]]; then
+  source ${BASEDIR}/deploy.sh
+  startDeployments ${CLUSTER} ${NAMESPACE}
+elif [[ "${KUBE_KIND}" == "job"]]; then
+  source ${BASEDIR}/job.sh
+  startJob ${CLUSTER} ${NAMESPACE}
+else
+  echo "[ERROR] Unsupported kubernetes kind: ${KUBE_KIND}"
+  echo "[INFO] Supported kinds: [ deployment, job ]"
+  exit 1
+fi
