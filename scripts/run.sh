384121f85d27958c60dc96ec98139957278b4ae1
Giridaran <giri@razorpay.com>
2019-02-25 13:41:32 +0530

    [f_multi_cluster] Fixes multi cluster substitution. Moves exit 0 after script is done testing for polling.

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,9 +6,11 @@ BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )
 source "${BASEDIR}/params.sh"
 source "${BASEDIR}/cluster-auth.sh"
 
+# Set clusters as csv for eg. : kluster-api,cde-green
+IFS=',' read -ra CLUSTERS <<< "$PLUGIN_CLUSTER" 
+
 # Set global params
-for i in ${PLUGIN_CLUSTER}
-do
+for i in "${CLUSTERS[@]}"; do
     setGlobals $i
     # Source the right script for kind
     source "${BASEDIR}/${KUBE_KIND,,}.sh"
@@ -16,9 +18,11 @@ do
     clusterAuth "${SERVER_URL}" "${CLUSTER}" "${USER}"
     setContext "${CLUSTER}" "${USER}"
     
-    if [[ ${KUBE_KIND} == "DEPLOYMENT" ]]; then
+    if [[ ${KUBE_KIND} == "DEPLOYMENT" ]]; then 
         startDeployments "${CLUSTER}" "${NAMESPACE}"
-        elif [[ ${KUBE_KIND} == "DAEMONSET" ]]; then
+    elif [[ ${KUBE_KIND} == "DAEMONSET" ]]; then
         startDaemonsets "${CLUSTER}" "${NAMESPACE}"
     fi
 done
+
+exit 0
